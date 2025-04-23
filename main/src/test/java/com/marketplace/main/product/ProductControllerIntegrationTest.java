d342db5da6f55de9d37e616d7606043112e518f2
HaaaamBurger <gemasclashes@gmail.com>
2025-04-23 11:35:22 +0300

    #1118: refactoring

--- main/src/test/java/com/marketplace/main/product/ProductControllerIntegrationTest.java
+++ main/src/test/java/com/marketplace/main/product/ProductControllerIntegrationTest.java
@@ -57,11 +57,7 @@ class ProductControllerIntegrationTest {
     void setUp() {
         applicationContext.getBeansOfType(MongoRepository.class)
                 .values()
-                .forEach(repo -> {
-                    if (repo != null) {
-                        repo.deleteAll();
-                    }
-                });
+                .forEach(MongoRepository::deleteAll);
     }
 
     @Test
@@ -79,12 +75,11 @@ class ProductControllerIntegrationTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse().getContentAsString();
 
-        List<Product> products = objectMapper.readValue(response, new TypeReference<>() {
-        });
+        List<Product> responseProducts = objectMapper.readValue(response, new TypeReference<>() {});
 
-        assertThat(products).isNotNull();
-        assertThat(products.size()).isEqualTo(1);
-        assertThat(products.get(0).getId()).isEqualTo(product.getId());
+        assertThat(responseProducts).isNotNull();
+        assertThat(responseProducts.size()).isEqualTo(1);
+        assertThat(responseProducts.get(0).getId()).isEqualTo(product.getId());
     }
 
     @Test
@@ -96,13 +91,13 @@ class ProductControllerIntegrationTest {
         productRepository.save(product);
 
         String accessToken = jwtService.generateAccessToken(user);
-        String contentAsString = mockMvc.perform(get("/product/" + product.getId())
+        String response = mockMvc.perform(get("/product/" + product.getId())
                         .header(AUTHORIZATION_HEADER, BEARER_PREFIX + accessToken))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse().getContentAsString();
 
-        Product responseProduct = objectMapper.readValue(contentAsString, Product.class);
+        Product responseProduct = objectMapper.readValue(response, Product.class);
 
         assertThat(responseProduct).isNotNull();
         assertThat(responseProduct.getId()).isEqualTo(product.getId());
@@ -112,12 +107,10 @@ class ProductControllerIntegrationTest {
     @Test
     void createProduct_ShouldReturnCreatedProduct() throws Exception {
         User user = UserDataBuilder.buildUserWithAllFields().build();
+        Product product = ProductDataBuilder.buildProductWithAllFields().build();
 
         userRepository.save(user);
-
         String accessToken = jwtService.generateAccessToken(user);
-
-        Product product = ProductDataBuilder.buildProductWithAllFields().build();
 
         String response = mockMvc.perform(post("/product/create")
                         .header(AUTHORIZATION_HEADER, BEARER_PREFIX + accessToken)
@@ -138,20 +131,14 @@ class ProductControllerIntegrationTest {
     @Test
     void updateProduct_ShouldUpdateAndReturnProduct() throws Exception {
         User user = UserDataBuilder.buildUserWithAllFields().build();
-
-        userRepository.save(user);
-
-        String accessToken = jwtService.generateAccessToken(user);
-
         Product product = ProductDataBuilder.buildProductWithAllFields().build();
-
-        product = productRepository.save(product);
-
         Product updatedProduct = ProductDataBuilder.buildProductWithAllFields()
                 .name("Updated Product")
-                .description("Updated Desc")
-                .price(BigDecimal.valueOf(123.45))
                 .build();
+
+        userRepository.save(user);
+        product = productRepository.save(product);
+        String accessToken = jwtService.generateAccessToken(user);
 
         String response = mockMvc.perform(put("/product/" + product.getId())
                         .header(AUTHORIZATION_HEADER, BEARER_PREFIX + accessToken)
@@ -163,21 +150,18 @@ class ProductControllerIntegrationTest {
 
         Product responseProduct = objectMapper.readValue(response, Product.class);
 
+        assertThat(product.getId()).isEqualTo(responseProduct.getId());
         assertThat(responseProduct.getName()).isEqualTo("Updated Product");
-        assertThat(responseProduct.getDescription()).isEqualTo("Updated Desc");
-        assertThat(responseProduct.getPrice()).isEqualTo(BigDecimal.valueOf(123.45));
     }
 
     @Test
     void deleteProduct_ShouldRemoveProduct() throws Exception {
         User user = UserDataBuilder.buildUserWithAllFields().build();
+        Product product = ProductDataBuilder.buildProductWithAllFields().build();
 
         userRepository.save(user);
-
-        String accessToken = jwtService.generateAccessToken(user);
-
-        Product product = ProductDataBuilder.buildProductWithAllFields().build();
         product = productRepository.save(product);
+        String accessToken = jwtService.generateAccessToken(user);
 
         assertThat(productRepository.findById(product.getId())).isPresent();
 
@@ -185,6 +169,6 @@ class ProductControllerIntegrationTest {
                         .header(AUTHORIZATION_HEADER, BEARER_PREFIX + accessToken))
                 .andExpect(status().isOk());
 
-        assertThat(productRepository.findById(product.getId())).isEmpty();
+        assertThat(productRepository.findById(product.getId())).isNotPresent();
     }
 }
