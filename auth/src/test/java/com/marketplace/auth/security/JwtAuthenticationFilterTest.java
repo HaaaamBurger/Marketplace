81bb46623a4461b0ebc6138f1015c4274ca6057e
HaaaamBurger <gemasclashes@gmail.com>
2025-06-16 12:02:11 +0300

    #1144: JwtAuthenticationFilterTest v2

--- auth/src/test/java/com/marketplace/auth/security/JwtAuthenticationFilterTest.java
+++ auth/src/test/java/com/marketplace/auth/security/JwtAuthenticationFilterTest.java
@@ -1,10 +1,15 @@
 package com.marketplace.auth.security;
 
 import com.marketplace.auth.config.AuthApplicationConfig;
+import com.marketplace.auth.security.cookie.CookieNotFoundException;
 import com.marketplace.auth.security.cookie.CookieService;
+import com.marketplace.auth.security.token.JwtService;
+import com.marketplace.auth.security.token.TokenPayload;
 import com.marketplace.auth.service.JwtCookieManager;
 import com.marketplace.auth.service.JwtTokenManager;
 import com.marketplace.usercore.model.User;
+import com.marketplace.usercore.model.UserStatus;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -24,6 +29,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import java.io.IOException;
 
 import static com.marketplace.auth.security.cookie.CookieService.COOKIE_ACCESS_TOKEN;
+import static com.marketplace.auth.security.cookie.CookieService.COOKIE_REFRESH_TOKEN;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -47,6 +53,12 @@ public class JwtAuthenticationFilterTest {
     private HttpServletResponse httpServletResponse;
 
     @MockitoBean
+    private SecurityContextHolder securityContextHolder;
+
+    @MockitoBean
+    private JwtService jwtService;
+
+    @MockitoBean
     private FilterChain filterChain;
 
     @Autowired
@@ -54,32 +66,160 @@ public class JwtAuthenticationFilterTest {
 
     @Test
     public void doFilterInternal_ShouldSuccessfullyValidateToken() throws ServletException, IOException {
-        MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class);
-        SecurityContext mockedSecurityContext = mock(SecurityContext.class);
-        Cookie mockedCookie = mock(Cookie.class);
-        String accessToken = "accessToken";
-        User mockedUser = mock(User.class);
+        try (MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class)) {
+            SecurityContext mockedSecurityContext = mock(SecurityContext.class);
+            Cookie mockedCookie = mock(Cookie.class);
+            String accessToken = "accessToken";
+            User mockedUser = mock(User.class);
 
-        mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
-        when(mockedSecurityContext.getAuthentication()).thenReturn(null);
-        when(cookieService.extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest)).thenReturn(mockedCookie);
-        when(mockedCookie.getValue()).thenReturn(accessToken);
-        when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(accessToken)).thenReturn(mockedUser);
+            mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
 
-        jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+            when(mockedSecurityContext.getAuthentication()).thenReturn(null);
+            when(cookieService.extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest)).thenReturn(mockedCookie);
+            when(mockedCookie.getValue()).thenReturn(accessToken);
+            when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(accessToken)).thenReturn(mockedUser);
 
-        ArgumentCaptor<Authentication> authenticationArgumentCaptor = ArgumentCaptor.forClass(Authentication.class);
+            jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
 
-        verify(mockedSecurityContext).getAuthentication();
-        verify(cookieService).extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest);
-        verify(jwtTokenManager).getUserDetailsIfTokenValidOrThrow(accessToken);
+            ArgumentCaptor<Authentication> authenticationArgumentCaptor = ArgumentCaptor.forClass(Authentication.class);
 
-        verify(mockedSecurityContext).setAuthentication(authenticationArgumentCaptor.capture());
-        assertThat(authenticationArgumentCaptor.getValue()).isNotNull();
-        assertThat(authenticationArgumentCaptor.getValue().isAuthenticated()).isTrue();
+            verify(mockedSecurityContext).getAuthentication();
+            verify(cookieService).extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest);
+            verify(mockedCookie).getValue();
+            verify(jwtTokenManager).getUserDetailsIfTokenValidOrThrow(accessToken);
 
-        verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+            verify(mockedSecurityContext).setAuthentication(authenticationArgumentCaptor.capture());
+            assertThat(authenticationArgumentCaptor.getValue()).isNotNull();
+            assertThat(authenticationArgumentCaptor.getValue().isAuthenticated()).isTrue();
 
-        mockedSecurityContextHolder.close();
+            verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
+
+    @Test
+    public void doFilterInternal_ShouldSuccessfullyValidateToken_WhenContextAlreadyExists() throws ServletException, IOException {
+        try (MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class)) {
+            Authentication mockedAuthentication = mock(Authentication.class);
+            SecurityContext mockedSecurityContext = mock(SecurityContext.class);
+
+            mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
+
+            when(mockedSecurityContext.getAuthentication()).thenReturn(mockedAuthentication);
+
+            jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+
+            verify(mockedSecurityContext).getAuthentication();
+            verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
+
+    @Test
+    public void doFilterInternal_ShouldRedirectToSignInPage_WhenTokenInvalidExceptionAndRefreshNotValid() throws ServletException, IOException {
+        try (MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class)) {
+            SecurityContext mockedSecurityContext = mock(SecurityContext.class);
+            Cookie mockedCookie = mock(Cookie.class);
+            String accessToken = "accessToken";
+            String refreshToken = "refreshToken";
+
+            mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
+
+            when(mockedSecurityContext.getAuthentication()).thenReturn(null);
+            when(cookieService.extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest)).thenReturn(mockedCookie);
+            when(mockedCookie.getValue()).thenReturn(accessToken);
+            when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(accessToken)).thenThrow(JwtException.class);
+            when(cookieService.extractCookieByName(COOKIE_REFRESH_TOKEN, httpServletRequest)).thenReturn(mockedCookie);
+            when(mockedCookie.getValue()).thenReturn(refreshToken);
+            when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(refreshToken)).thenThrow(JwtException.class);
+
+            jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+
+            verify(mockedSecurityContext).getAuthentication();
+            verify(cookieService).extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest);
+            verify(cookieService).extractCookieByName(COOKIE_REFRESH_TOKEN, httpServletRequest);
+            verify(mockedCookie, times(2)).getValue();
+            verify(jwtTokenManager, times(2)).getUserDetailsIfTokenValidOrThrow(refreshToken);
+            verify(httpServletResponse).sendRedirect("/sign-in");
+        }
+    }
+
+    @Test
+    public void doFilterInternal_ShouldSuccessfullyValidateToken_WhenTokenInvalidExceptionAndRefreshIsValid() throws ServletException, IOException {
+        try (MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class)) {
+            SecurityContext mockedSecurityContext = mock(SecurityContext.class);
+            Cookie mockedAccessCookie = mock(Cookie.class);
+            Cookie mockedRefreshCookie = mock(Cookie.class);
+            String accessToken = "accessToken";
+            String refreshToken = "refreshToken";
+            User mockedUser = mock(User.class);
+            TokenPayload mockedTokenPayload = mock(TokenPayload.class);
+
+            mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
+
+            when(mockedSecurityContext.getAuthentication()).thenReturn(null);
+            when(cookieService.extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest)).thenReturn(mockedAccessCookie);
+            when(mockedAccessCookie.getValue()).thenReturn(accessToken);
+            when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(accessToken)).thenThrow(JwtException.class);
+            when(cookieService.extractCookieByName(COOKIE_REFRESH_TOKEN, httpServletRequest)).thenReturn(mockedRefreshCookie);
+            when(mockedRefreshCookie.getValue()).thenReturn(refreshToken);
+            when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(refreshToken)).thenReturn(mockedUser);
+            when(jwtTokenManager.generateTokenPayload(mockedUser)).thenReturn(mockedTokenPayload);
+
+            jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+
+            verify(mockedSecurityContext).getAuthentication();
+            verify(cookieService).extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest);
+            verify(mockedAccessCookie).getValue();
+            verify(jwtTokenManager).getUserDetailsIfTokenValidOrThrow(accessToken);
+            verify(cookieService).extractCookieByName(COOKIE_REFRESH_TOKEN, httpServletRequest);
+            verify(mockedRefreshCookie).getValue();
+            verify(jwtTokenManager).getUserDetailsIfTokenValidOrThrow(refreshToken);
+            verify(jwtTokenManager).generateTokenPayload(mockedUser);
+
+            verify(httpServletResponse, never()).sendRedirect("/sign-in");
+        }
+    }
+
+    @Test
+    public void doFilterInternal_ShouldRedirectToErrorPage_WhenUserBlocked() throws ServletException, IOException {
+        try (MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class)) {
+            SecurityContext mockedSecurityContext = mock(SecurityContext.class);
+            Cookie mockedCookie = mock(Cookie.class);
+            String accessToken = "accessToken";
+            User mockedUser = mock(User.class);
+
+            mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
+
+            when(mockedSecurityContext.getAuthentication()).thenReturn(null);
+            when(cookieService.extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest)).thenReturn(mockedCookie);
+            when(mockedCookie.getValue()).thenReturn(accessToken);
+            when(jwtTokenManager.getUserDetailsIfTokenValidOrThrow(accessToken)).thenReturn(mockedUser);
+            when(mockedUser.getStatus()).thenReturn(UserStatus.BLOCKED);
+
+            jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+
+            verify(mockedSecurityContext).getAuthentication();
+            verify(cookieService).extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest);
+            verify(mockedCookie).getValue();
+            verify(jwtTokenManager).getUserDetailsIfTokenValidOrThrow(accessToken);
+            verify(jwtCookieManager).deleteTokensFromCookie(httpServletResponse);
+        }
+    }
+
+    @Test
+    public void doFilterInternal_ShouldSuccessfullyValidateToken_WhenNoCookie() throws ServletException, IOException {
+        try (MockedStatic<SecurityContextHolder> mockedSecurityContextHolder = mockStatic(SecurityContextHolder.class)) {
+            SecurityContext mockedSecurityContext = mock(SecurityContext.class);
+
+            mockedSecurityContextHolder.when(SecurityContextHolder::getContext).thenReturn(mockedSecurityContext);
+
+            when(mockedSecurityContext.getAuthentication()).thenReturn(null);
+            when(cookieService.extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest)).thenThrow(CookieNotFoundException.class);
+
+            jwtAuthenticationFilter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+
+            verify(mockedSecurityContext).getAuthentication();
+            verify(cookieService).extractCookieByName(COOKIE_ACCESS_TOKEN, httpServletRequest);
+            verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        }
     }
 }
