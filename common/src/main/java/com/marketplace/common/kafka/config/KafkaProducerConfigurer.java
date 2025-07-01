6ceb9280ca1df9473e59cbc38b2131f55e3bad50
HaaaamBurger <gemasclashes@gmail.com>
2025-07-01 10:00:20 +0300

    #1155: refactoring

--- common/src/main/java/com/marketplace/common/kafka/config/KafkaProducerConfigurer.java
+++ common/src/main/java/com/marketplace/common/kafka/config/KafkaProducerConfigurer.java
@@ -2,7 +2,6 @@ package com.marketplace.common.kafka.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,8 +13,7 @@ import java.util.HashMap;
 @Configuration
 public class KafkaProducerConfigurer {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String KAFKA_BOOTSTRAP_SERVERS;
+    private static final String KAFKA_BOOTSTRAP_SERVERS = "localhost:9092";
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
